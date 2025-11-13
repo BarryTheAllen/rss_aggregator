de3e2aa916abe984fb4ea9832a1dbad14088af74
BarryTheAllen <radyga936@gmail.com>
2025-11-13 05:42:36 +0400

    styling

--- frontend/rssGenerator/src/widgets/FeedControls/ui/FeedControls.jsx
+++ frontend/rssGenerator/src/widgets/FeedControls/ui/FeedControls.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import AddRssURL from "./AddRssURL";
 import styles from "./FeedControls.module.css";
 import TagList from "./TagList";
-
 const FeedControls = () => {
+  const [isMobileState, setIsMobileState] = useState(false);
+  useEffect(() => {
+    const isMobile = window.innerWidth < 768;
+    setIsMobileState(isMobile);
+  }, []);
+
   return (
     <aside className={styles.feedControls}>
-      <AddRssURL />
-      <TagList />
+      {isMobileState ? (
+        <>
+          <div>asdasdasdasdas</div>
+          <AddRssURL />
+          <TagList />
+        </>
+      ) : (
+        <>
+          <AddRssURL />
+          <TagList />
+        </>
+      )}
     </aside>
   );
 };
