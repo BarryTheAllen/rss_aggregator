88ef3ec76d0e9326f070b3f7ebf62064c6307ce2
BarryTheAllen <radyga936@gmail.com>
2025-11-15 08:37:58 +0400

    rebuild logo component

--- rss_aggregator/src/shared/UI/Logo/Logo.jsx
+++ rss_aggregator/src/shared/UI/Logo/Logo.jsx
@@ -4,10 +4,8 @@ import logo from "./assets/logo.png";
 const Logo = () => {
   return (
     <div className={styles.logo}>
-      <div className={styles.block}>
-        <img src={logo} alt="logo" className={styles.logoImg} />
-        <h1 className={styles.logoTitle}>RSS</h1>
-      </div>
+      <img src={logo} alt="logo" className={styles.logoImg} />
+      <h1 className={styles.logoTitle}>RSS</h1>
     </div>
   );
 };
